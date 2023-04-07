d69cd5dbd4bf0f26a3550a654f21df37d47bab41
dome877 <116706208+dome877@users.noreply.github.com>
2023-04-07 18:25:43 +0200

    do 4.og

---
+++ IspitNapredno/Narudzba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IspitNapredno
+{
+    internal class Narudzba
+    {
+        public delegate void PredajNarudzbu(Narudzba narudzba);
+
+        public event PredajNarudzbu NarudzbaPredana;
+        public string Naziv { get; set; }
+        
+        public void KreirajNarudzbu()
+        {
+            Console.WriteLine("Narudzba {0} je narucena", this.Naziv);
+            NarudzbaPredana?.Invoke(this);
+        }
+
+    }
+}
---
+++ IspitNapredno/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IspitNapredno
+{
+    public delegate void NarudzbaNarucena(DateTime datumivrijeme);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
